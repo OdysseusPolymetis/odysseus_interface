0946646ec8884cd5526238b6c0f1919f8132b9a3
OdysseusPolymetis <odysseuspolymetis2010@gmail.com>
2021-07-09 21:56:07 +0200

    public_domain

--- README.md
+++ README.md
@@ -1,1 +1,31 @@
-# odysseus_interface
+# ParaLogos
+
+[en] An online alignment of several French translations of Homer's Odyssey
+
+[fr] Un lecteur en ligne de traductions de l’Odyssée.
+
+Cette interface javascript prend en entrée la sortie HTML du logiciel Odysseus sur ce site, et met en valeur l’information sous la forme de colonnes synchronisées. L’idée peut être transposée mais le code est très lié à la structure de l’Odyssée, à la division en chants et à une politique de nommage des fichiers. Structure du projet :
+
+* **html/** fichiers de chapitres de l’odyssée selon le format codeLivre_{no chapitre}.html
+* **biblio.js** métadonnées des livres structurées en json
+
+Interface pour l’instant tout javascript sans dépendance à une librairie javascript ou à un langage serveur type Javascript. Démonstration : http://odysseuspolymetis.github.io/paralogos/
+
+# TODO
+
+* Prioriser ce TODO
+* Renommer Odysseus.js en Paralogos.js ?
+* Expliquer un peu plus de quoi il s’agit sur la page d’accueil (légende des couleurs)
+* Mettre à jour et vérifier biblio.js.
+* Faut-il afficher toutes les traductions dès l’accueil au public ou juste une sélection ? (implémentrer cette sélection)
+* Ajouter une colonne après la colonne active (et pas tout au bout)
+* Glisser/déposer les colonnes
+* Meilleures couleurs pour les happax
+* Vue vers ou sans vers
+* Côté Odysseus, conserver les clitiques « elle avait pris le aspect de… » (bof)
+* Vue pour la publi conservant l’apparence en vers (côté Odysseus, passer les / avec un balise <br/> ?)
+
+# DONE
+
+* isoler biblio.js
+* écriture plus modulaire de l’objet Odysseus.js
---
+++ biblio.js
@@ -0,0 +1,143 @@
+﻿// Création conditionnelle de l’objet global
+var Odysseus = Odysseus || {};
+// la biblio, l’ordre n’est pas significatif, il est en ordre alphabétique des identifiants
+// pour ne pas s’y perdre
+// le chargement initial se fait en ordre de date
+Odysseus.corpus = {
+  "bareste1842": {
+    date: 1842,
+    creator: "Eugène Bareste",
+    title: "Odyssée. Traduction nouvelle accompagnée de notes, d'explications et de commentaires",
+  },
+  "berard1924": {
+    date: 1924,
+    creator: "Victor Bérard",
+    title: "Les navigations d’Ulysse",
+  },
+  "bignan1853": {
+    date: 1841,
+    creator: "Anne Bignan",
+    title: "L’Odyssée",
+  },
+  "bitaube1810": {
+    date: 1785,
+    creator: "Paul Jérémie Bitaubé",
+    title: "L’Odyssée [édition de 1810]",
+  },
+  "boitel1619": {
+	    date: 1619,
+	    creator: "Boitel",
+	    title: "L’Odyssée",
+	  },
+  "calbetrosny1897": {
+    date: 1897,
+    creator: "Calbet Rosny",
+    title: "L’Odyssée",
+  },
+  "certon1604": {
+    date: 1604,
+    creator: "Salomon Certon",
+    title: "L’Odyssée",
+  },
+  "dacier1872": {
+    date: 1716,
+    creator: "Anne Dacier",
+    title: "L'Iliade et l'Odyssée. Traduction de Mme Dacier. Nouvelle édition revue, corrigée [édition de 1872]",
+  },
+  "dugasmontbel1833": {
+    date: 1833,
+    creator: "Jean-Baptiste Dugas-Montbel",
+    title: "L’Odyssée d’Homère",
+  },
+  "dufourraison1946": {
+    date: 1946,
+    creator: "Médéric Dufour et Jeanne Raison",
+    title: "L’Odyssée d’Homère",
+  },
+  "froment1883": {
+    date: 1883,
+    creator: "J.B.F. Froment",
+    title: "Homère",
+  },
+  "giguet1852": {
+    date: 1852,
+    creator: "P. Giguet",
+    title: "L’Iliade et l’Odyssée",
+  },
+  "odyssee1000": {
+    date: 1000,
+    creator: "Homère",
+    title: "Odyssée",
+  },
+  "hins1883": {
+    date: 1883,
+    creator: "Eugène Hins",
+    title: "L'Odyssée",
+  },
+  "odysseelat1000": {
+    date: 1589,
+    creator: "Henri Estienne",
+    title: "Odyssée Latine",
+  },
+  "jaccottet1955": {
+    date: 1955,
+    creator: "Philippe Jaccottet",
+    title: "L’Odyssée",
+  },
+  "jamyn1584": {
+    date: 1584,
+    creator: "Amadis Jamyn",
+    title: "L’Odyssée",
+  },
+  "lavalterie1681": {
+    date: 1681,
+    creator: "Achille de La Valterie",
+    title: "L’Odyssée d’Homère",
+    section: [1, 2, 3, 4],
+  },
+  "lcdlisle1867": {
+    date: 1867,
+    creator: "Leconte de Lisle",
+    title: "L’Odyssée",
+  },
+  "lplbrun1819": {
+    date: 1819,
+    creator: "Charles François Lebrun",
+    title: "L’Odyssée d’Homère",
+  },
+  "meunier1943": {
+    date: 1943,
+    creator: "Mario Meunier",
+    title: "Odyssée",
+  },
+  "mugler1991": {
+    date: 1991,
+    creator: "Frédéric Mugler",
+    title: "L’odyssée",
+  },
+  "peletierdumans1540": {
+    date: 1540,
+    creator: "Jacques Peletier du Mans",
+    title: "L’Odyssée",
+  },
+  "pessonneaux1866": {
+    date: 1862,
+    creator: "Émile Pessonneaux",
+    title: "L’Iliade et l’Odyssée",
+  },
+  "rochefort1777": {
+    date: 1777,
+    creator: "Guillaume Dubois de Rochefort",
+    title: "Odyssée",
+  },
+  "seguier1896": {
+    date: 1896,
+    creator: "Ulysse de Séguier",
+    title: "L'Odyssée",
+  },
+  "sommer1886": {
+    date: 1886,
+    creator: "Édouard Sommer",
+    title: "L’Odyssée en juxtalinéaire",
+  },
+};
